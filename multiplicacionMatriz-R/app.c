626d5d8154b707699b8e6354ba2f003e464c2f23
Saul Rovelo <sr031437@gmail.com>
2025-02-13 15:04:23 +0000

    Se codifican matrices

--- multiplicacionMatriz-R/app.c
+++ multiplicacionMatriz-R/app.c
@@ -1,1 +1,20 @@
-//Inicializacion del proyecto
+#include <stdio.h>   
+#include <pthread.h> // La usaremos para trabajar con hilos 
+
+#define N 3 // Constante para el tamaño de la matriz
+
+// Matriz de N * N, pero por simplicidad del ejercico haremos matrices de 3*3
+int A[N][N]= {
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+};
+
+int B[N][N] = {
+    {9, 8, 7},
+    {6, 5, 4},
+    {3, 2, 1}
+};
+
+// Matriz para almacenar el resultado
+int C[N][N];
