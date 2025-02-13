79543c335bab067af925cd0fa70c9b45a2660ff8
Saul Rovelo <sr031437@gmail.com>
2025-02-13 16:43:58 +0000

    Se agrega el main completo y funcional

--- multiplicacionMatriz-R/app.c
+++ multiplicacionMatriz-R/app.c
@@ -24,7 +24,7 @@ int C[N][N];
 void* multiplicacion(void* arg) {
     // Extraemos el argumento
     int fila = *(int*)arg;
-    //free(arg); // Liberamos la memoria 
+    
 
     // Recorremos todas las columnas de la fila
     for (int col = 0; col < N; col++) {
@@ -39,24 +39,37 @@ void* multiplicacion(void* arg) {
     return NULL; // Finalizamos
 }
 
-int main (){
+int main() {
+    pthread_t hilo0, hilo1, hilo2; // Declaramos los hilos 
 
-    pthread_t hilo0, hilo1, hilo2; // Declaramos los hilos
+    // Creamos los 3 hilos para cada una de las filas de A
 
     int fila0 = 0; // fila 1 
-
-    // Creamos el hilo para la fila 1 con sus argumentos correspondientes
+    //Creamos el hilo para la fila 1 con sus argumentos correspondientes
     pthread_create(&hilo0, NULL, multiplicacion, &fila0); 
 
+    // &hilo0: Puntero al identificador del hilo (Guardamos su referencia)
+    // NULL: porque usamos los atributos por defecto del hilo 
+    // multiplicacion: Le pasamos la funcion para ejecutar 
+    // &fila0: Pasamos por referencia el numero de la fila 
+
+
+    int fila1 = 1; // fila 2
+    //Creamos el hilo para la fila 2 con sus argumentos correspondientes
+    pthread_create(&hilo1, NULL, multiplicacion, &fila1); 
+
     
+    int fila2 = 2; // fila 3
+    //Creamos el hilo para la fila 3 con sus argumentos correspondientes
+    pthread_create(&hilo2, NULL, multiplicacion, &fila2); 
 
-    // Esperamos a que el hilo termine
+    // Esperamos a que los hilos terminen
     pthread_join(hilo0, NULL);
-
-   
+    pthread_join(hilo1, NULL);
+    pthread_join(hilo2, NULL);
 
     // For que recorre la matriz resultado para imprimir
-    printf("Resultado C:\n");
+    printf("Resultado de la matriz C:\n");
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             printf("%d ", C[i][j]);
@@ -64,5 +77,6 @@ int main (){
         printf("\n");
     }
 
-    return 0;
+    return 0; 
 }
+
