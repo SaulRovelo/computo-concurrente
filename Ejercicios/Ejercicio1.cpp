44e91b3da3fb625acea8a3e37d5d42066219290f
Saul Rovelo <sr031437@gmail.com>
2025-02-14 03:45:48 +0000

    Se agregan comentarios

--- Ejercicios/Ejercicio1.cpp
+++ Ejercicios/Ejercicio1.cpp
@@ -3,13 +3,18 @@
 #include <thread>
 #include <chrono>
 
-int pc=0;
+int pc=0; //Representa el numero de la instruccion
+
 
 void ejecutaInstruccion(std::string instruccion) {
-    std::cout << "PC: " << pc << " Ejecutando: " << instruccion << "\n";  
-    pc++;
-    std::this_thread::sleep_for(std::chrono::milliseconds(500)); 
+    
+    std::cout << "PC: " << pc << " Ejecutando: " << instruccion << "\n";  //Imprimimos la instruccion actual
+    pc++; //Pasamos a la siguiente instruccion 
+    std::this_thread::sleep_for(std::chrono::milliseconds(500)); //Ponmeos 500 milisegundos, simulando 
+                                                                //el tiempo de procesamiento de una instruccion
 }
+// Usamos std::this_thread::sleep_for porque es más preciso y funciona en cualquier sistema operativo.
+// sleep(segundos) solo acepta enteros y no es portable en Windows.
 
 
 int main() {
@@ -21,11 +26,22 @@ int main() {
         "Multiplicar los datos del proceso B",
         "Sumar los resultados del proceso A con los datos del proceso B"
     };
+    // Se usa std::vector<std::string> para almacenar una lista de instrucciones en un arreglo dinamico.
+    // Cada instrucción se guarda en un índice del vector, como en un array 
+    // Se puede acceder con instrucciones[i], lo que quiere decir que cada instruccion tiene un indice
 
     
     for (auto& instruccion : instrucciones) {  
         ejecutaInstruccion(instruccion);  
+
     }
 
-    return 0;
+    // Se lee como: "Para cada elemento en instrucciones, guardalo en instruccion y ejecuta el bloque de codigo".
+    // - tipo: Se usa auto para deducir el tipo automaticamente.
+    // - &: Indica que se pasa por referencia, evitando copias innecesarias.
+    // - instruccion: Representa cada elemento del contenedor en cada iteración.
+    // - instrucciones: Es la estructura a recorrer (vector, array, lista, etc.).
+
+
+    return 0; //Finalizacion
 }
