eed5f61152ae2f301097ddc847a3e0b2e2a69b3a
Saúl-Rovelo <sr031437@gmail.com>
2025-02-16 09:12:54 +0000

    Se añaden comentarios

--- Ejercicios/Ejercicio5.c
+++ Ejercicios/Ejercicio5.c
@@ -3,33 +3,48 @@
 #include <time.h>
 #include <unistd.h>
 
+
+// Variable global contador (se podría usar sin ser global, pero se pasa por referencia)
 int contador;
 
-void * muestraContador(void *arg) {
-    int *contador = (int *)arg; // Casteamos
+// Funcion que ejecuta el hilo secundario
+void *muestraContador(void *arg) {
+    int *contador = (int *)arg; // Casteo del argumento a un puntero a entero
+    
+    
     while (*contador < 10) {
         printf("\nEl contador es: %d\n", *contador);
-        sleep(1);  
+        sleep(1);  // Espera un segundo antes de la siguiente impresión
     }
-    return NULL;
+    
+    return NULL; 
 }
 
 int main() {
-    int contador = 0;
-    pthread_t hilo1;
+    int contador = 0; // Variable compartida 
+    pthread_t hilo1;  //Declaramos el hilo
 
+    // Creacion del hilo secundario que ejecutara la función muestraContador
     pthread_create(&hilo1, NULL, muestraContador, &contador);
 
-   
+    // Bucle en el hilo principal que incrementa el contador
     for (int i = 0; i < 10; i++) {
         printf("sumando el contador\n");
-        contador += 1;
-        sleep(1);  
+        contador += 1; // Incrementamos el contador 
+        sleep(1);  // Se espera un segundo antes de la siguiente suma
     }
 
     printf("Finalizamos\n");
 
-    pthread_join(hilo1, NULL); 
+    // Esperamos a que el hilo secundario termine antes de finalizar el programa
+    pthread_join(hilo1, NULL);
 
     return 0;
 }
+
+/*
+    El programa simula un proceso donde un hilo secundario (hilo1) muestra 
+    el valor de una variable compartida (contador) mientras el hilo principal(main)
+    la incrementa. El contador se incrementa hasta un límite de 10, 
+    y el hilo secundario imprime su valor en cada iteración.
+*/
