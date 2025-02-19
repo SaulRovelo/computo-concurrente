bb5de5ef608e7208a0d15693bc7c8dfa3ed13c21
Saúl-Rovelo <sr031437@gmail.com>
2025-02-19 03:29:04 +0000

    Se agrega funcion brinca

--- CarreraRanitas-PT/app.cpp
+++ CarreraRanitas-PT/app.cpp
@@ -6,7 +6,16 @@
 #define INICIO 1
 #define META 30
 
-void brinca();
+void brinca(void * arg){
+    int * id = (int *) arg; //casteamos
+    int posicion = INICIO; 
+    while (posicion<META) //Cuando la ranita llegue a la meta que es 30 se sale del bucle
+    {
+        posicion+= rand()%10+1; //genera numeros entre 1 y 10
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));  // Simula el tiempo de ejecución de la instrucción
+    }
+    std::cout << "He llegado a la meta y soy la ranita " << *id << std::endl; //Imprimimos que la ranita a llegado a la meta
+}
 
 int main(int argc, char* argv[]){
 
@@ -26,12 +35,9 @@ int main(int argc, char* argv[]){
 
     //Esperamos a que todos los hilos terminen antes de finalizar el programa
     //(que las ranitas lleguen a la meta)
-
     for (int i = 0; i < ranas; i++)
-    {
-        
         ranitas[i].join();
-    }
+    
     
     
     
